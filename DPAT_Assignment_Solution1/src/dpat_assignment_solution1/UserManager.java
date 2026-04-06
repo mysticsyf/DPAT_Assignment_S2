8db84ecd398b8d34871cf5eaa6e72f3244c465ec
Yifeng <simyifeng649@gmail.com>
2026-04-06 21:22:51 +0800

    new update

--- DPAT_Assignment_Solution1/src/dpat_assignment_solution1/UserManager.java
+++ DPAT_Assignment_Solution1/src/dpat_assignment_solution1/UserManager.java
@@ -12,7 +12,7 @@ import java.io.*;
 
 public class UserManager {
 
-    private String userFile = "users.txt";
+    private String userFile = "users/users.txt";
 
     public UserManager() {
         initUsers();
